4ff13d74e3690308ae33ebdb419cc345591d6072
rusty <dalegribble@riseup.net>
2023-02-04 16:41:33 -0800

    fix chain id err message

--- components/mint.jsx
+++ components/mint.jsx
@@ -101,7 +101,7 @@ export default class Minting extends React.Component {
 	async startApp(provider) {
 		const chainId = await ethereum.request({ method: 'eth_chainId' })
 		if (chainId != 0x1E14) {
-			this.throwError("Please switch to Canto testnetwork.")
+			this.throwError("Please switch to Canto network.")
 			return
 		}
 
